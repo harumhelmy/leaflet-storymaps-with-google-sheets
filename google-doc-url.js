6b3af3394873035638b1631a091a34c0b311ae1e
Harum! <harumendhah@gmail.com>
2020-01-08 15:26:07 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/u/2/d/1Q-E-9kAiEx-vFoFG01aW2Bw1CLouKX1AZCipPe-Bu-U/edit#gid=0';
